2b2ab115566e5092bb86f69d5a688380026d368c
Eunyoung KANG <greenkey20@gmail.com>
2023-06-08 00:13:48 +0900

    fix: initialize/destroy with annotations

--- src/main/java/com/codestates/section2week4/sandwichprincess/lifecycle/TestClient.java
+++ src/main/java/com/codestates/section2week4/sandwichprincess/lifecycle/TestClient.java
@@ -3,6 +3,9 @@ package com.codestates.section2week4.sandwichprincess.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 // 2023.6.7(수) 23h45
 
 /**
@@ -21,14 +24,18 @@ public class TestClient /*implements InitializingBean, DisposableBean*/ {
      *
      * @throws Exception
      */
+    // 방법1)
 //    @Override
 //    public void afterPropertiesSet() throws Exception {
 //        System.out.println("초기화 메서드 실행");
 //    }
-    // 2023.6.8(목) 0h5
+
+    // 2023.6.8(목) 0h5 방법2)
+    @PostConstruct // 2023.6.8(목) 0h10 방법3) 최신 Spring에서 가장 권장
     public void init() {
         System.out.println("초기화 메서드 init() 실행");
     }
+
 
     public void connect() {
         System.out.println("클라이언트를 " + url + "로 연결");
@@ -39,11 +46,13 @@ public class TestClient /*implements InitializingBean, DisposableBean*/ {
      *
      * @throws Exception
      */
+    // 방법1)
 //    @Override
 //    public void destroy() throws Exception {
 //        System.out.println("종료 메서드 실행");
 //    }
-    // 2023.6.8(목) 0h5
+    // 2023.6.8(목) 0h5 방법2)
+    @PreDestroy // 2023.6.8(목) 0h10 방법3) 최신 Spring에서 가장 권장
     public void close() {
         System.out.println("종료 메서드 close() 실행");
     }
